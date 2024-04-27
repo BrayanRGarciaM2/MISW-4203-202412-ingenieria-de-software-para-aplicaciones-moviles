4f8fd2699dc51aceced18e92086b74851fdc5663
milindr4123 <e.foreros@uniandes.edu.co>
2024-04-27 11:38:57 -0500

    first commit

---
+++ Vinilos/app/src/main/java/com/tsdc/vinilos/client/APIService.kt
@@ -0,0 +1,32 @@
+package com.tsdc.vinilos.client
+
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import retrofit2.http.GET
+
+data class Todo(
+    var userId: Int,
+    var id: Int,
+    var title: String,
+    var completed: Boolean
+)
+
+const val BASE_URL = "http://localhost:3000/"
+
+interface APIService {
+    @GET("musicians")
+    suspend fun getTodos(): List<Todo>
+
+    companion object {
+        var apiService: APIService? = null
+        fun getInstance(): APIService {
+            if (apiService == null) {
+                apiService = Retrofit.Builder()
+                    .baseUrl(BASE_URL)
+                    .addConverterFactory(GsonConverterFactory.create())
+                    .build().create(APIService::class.java)
+            }
+            return apiService!!
+        }
+    }
+}
