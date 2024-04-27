5fd79688583f646d049d5df7edcac201afbcf98f
BrayanRGarciaM2 <br.garciam1@uniandes.edu.co>
2024-04-27 16:23:17 -0500

    feature(header): Header finished

--- Vinilos/app/src/main/java/com/tsdc/vinilos/album/vista/AlbumDetailScreen.kt
+++ Vinilos/app/src/main/java/com/tsdc/vinilos/album/vista/AlbumDetailScreen.kt
@@ -1,17 +1,8 @@
 package com.tsdc.vinilos.album.vista
 
-import androidx.compose.foundation.layout.BoxWithConstraints
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.rememberScrollState
-import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
-import com.tsdc.vinilos.album.AlbumDetailContent
 import com.tsdc.vinilos.album.album2
 import com.tsdc.vinilos.album.repository.Album
 
@@ -25,23 +16,10 @@ fun AlbumDetailScreen(album: Album?) {
         }
     ) { paddingValues ->
         val scrollState = rememberScrollState()
-
-        Column(
-            Modifier
-                .fillMaxSize()
-                .padding(paddingValues)
-        ) {
-            BoxWithConstraints {
-                Surface(color = Color.Black) {
-                    Column(
-                        modifier = Modifier
-                            .fillMaxSize()
-                            .verticalScroll(scrollState)
-                    ) {
-                        AlbumDetailContent(album ?: album2)
-                    }
-                }
-            }
-        }
+        AlbumDetailContent(
+            album = album ?: album2,
+            paddingValues = paddingValues,
+            scrollState = scrollState
+        )
     }
 }
